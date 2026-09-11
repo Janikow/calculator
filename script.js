271b44efe2ebe6cc515ae89cacb9074fd8d7fbcb
Tem <160335817+Janikow@users.noreply.github.com>
2026-09-11 12:09:44 -0500

    kirk

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const button1 = document.getElementById("button1")
+const button1 = document.getElementById("button1");
+
+function lexample()
+{
+    const img = document.createElement("img");
+    img.scr = "image.jpg";
+    img.alt = "An Image";
+    img.width = 300;
+
+    document.getElementById("imageContainer").appendChild(img);
+}
+
+button1.addEventListener("click", lexample);
